81a01022dbfc636708460b5d8d2371d92f31d8bc
berg-cell <berg@bnel.com.br>
2026-04-23 14:06:16 -0300

    blocoController.js

--- src/controllers/blocoController.js
+++ src/controllers/blocoController.js
@@ -46,6 +46,7 @@ exports.buscarPorId = async (req, res) => {
 
 exports.criar = async (req, res) => {
   try {
+    console.log("CRIAR BLOCO payload:", JSON.stringify(req.body));
     const bloco = await BlocoModel.create(req.body, req.usuario.id);
     await AuditoriaModel.registrar({
       usuario_id: req.usuario.id, acao: "BLOCO_CRIADO",
